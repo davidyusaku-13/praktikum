1ad4a435a24e96fbc06e7d4640dccf3aaf3c9809
David Yusaku <davidyusaku13@gmail.com>
2022-06-07 09:32:17 +0700

    updating

--- kb2/Task2.java
+++ kb2/Task2.java
@@ -1,9 +1,9 @@
 public class Task2 {
   public static void main(String[] args) {
-    int[[][] arr = new int[10][10];
-    int c=0;
-    
-    while(c<10){
+    int[][] arr = new int[10][10];
+    int c = 0;
+
+    while (c < 10) {
 
     }
   }
